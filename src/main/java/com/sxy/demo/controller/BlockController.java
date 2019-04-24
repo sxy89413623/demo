03b374715c180808de2aa11f9c7fae3272c48682
sxy89413623 <sxyzcs0211*>
2019-04-24 20:41:35 +0800

    something

--- src/main/java/com/sxy/demo/controller/BlockController.java
+++ src/main/java/com/sxy/demo/controller/BlockController.java
@@ -1,23 +1,89 @@
 package com.sxy.demo.controller;
 
+import com.sxy.demo.api.BitcoinJsonRpcClient;
 import com.sxy.demo.dto.BlockDetailDTO;
 import com.sxy.demo.dto.BlockListDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.sxy.demo.po.Block;
+import com.sxy.demo.service.BlockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/block")
+@CrossOrigin
 public class BlockController {
 
-    @GetMapping("/getRecentBlocksById")
-    public List<BlockListDTO> getRecentBlocksById(@RequestParam Integer blockchainId){
-        return null;
-    }
 
+
+    @Autowired
+    private BitcoinJsonRpcClient bitcoinJsonRpcClient;
+
+
+    @Autowired
+    private BlockService blockService;
+
+    @Value("${blockchain.recentCount}")
+    private Integer recentCount;
+
+    @GetMapping("/getRecentBlocks")
+    public List<BlockListDTO> getRecentBlocks() throws Throwable {
+
+//        String bestBlockhash = bitcoinJsonRpcClient.getBestBlockhash();
+//        String tempBlockhash = bestBlockhash;
+//
+//        List<BlockListDTO> blockListDTOS = new LinkedList<>();
+//
+//        for (int i = 0; i < 5; i++) {
+//            JSONObject block = bitcoinApi.getNoTxBlock(tempBlockhash);
+//            BlockListDTO blockListDTO = new BlockListDTO();
+//            blockListDTO.setHeight(block.getInteger("height"));
+//            Long time = block.getLong("time");
+//            Date date = new Date(time * 1000);
+//            blockListDTO.setTime(date);
+//            blockListDTO.setTxSize(block.getJSONArray("tx").size());
+//            blockListDTO.setSizeOnDisk(block.getLong("size"));
+//            blockListDTOS.add(blockListDTO);
+//            tempBlockhash = block.getString("previousblockhash");
+//        }
+
+//       JSONObject chainInfo = bitcoinApi.getChainInfo();
+//        Integer height = chainInfo.getInteger("blocks");
+//        height -= 5;
+//        String blockHashByHeight = bitcoinJsonRpcClient.getBlockHashByHeight(height);
+//        JSONArray blockHeaders = bitcoinApi.getBlockHeaders(5, blockHashByHeight);
+//
+//        LinkedList<BlockListDTO> blockListDTOS = new LinkedList<>();
+//        for (int i = 4; i > -1; i--) {
+//            JSONObject block = blockHeaders.getJSONObject(i);
+//            BlockListDTO blockListDTO = new BlockListDTO();
+//            blockListDTO.setHeight(block.getInteger("height"));
+//            Long time = block.getLong("time");
+//            Date date = new Date(time * 1000);
+//            blockListDTO.setTime(date);
+//            //todo add size on disk
+//            blockListDTO.setTxSize(block.getInteger("nTx"));
+//            blockListDTOS.add(blockListDTO);
+//        }
+
+//        List<Block> blocks = blockMapper.selectRecent();
+        List<Block> blocks = blockService.selectRecent();
+        List<BlockListDTO> blockListDTOS = blocks.stream().map(block -> {
+            BlockListDTO blockListDTO = new BlockListDTO();
+            blockListDTO.setHeight(block.getHeight());;
+            blockListDTO.setTxSize(block.getTxSize());
+            blockListDTO.setSizeOnDisk(block.getSizeOnDisk());
+            return blockListDTO;
+        }).collect(Collectors.toList());
+
+        return blockListDTOS;
+
+
+//        bitcoinApi.getBlockHeaders(recentCount,bestBlockhash)
+    }
 
     @GetMapping("/getRecentBlocksByNameType")
     public List<BlockListDTO> getRecentBlocksByNameType(@RequestParam String name,
